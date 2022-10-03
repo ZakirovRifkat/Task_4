6ca9ca8fc212e0d2cddea5cca5190796b5a193a3
ZakirovRifkat <zakirov2323rifkat@gmail.com>
2022-10-03 21:04:23 +0300

    Добавьте файлы проекта.

---
+++ Task_4.cpp
@@ -0,0 +1,14 @@
+﻿#include <vector>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Задание 4. Разностный метод решения краевой задачи для ОДУ 2-го порядка.\n";
+    return 0;
+}
+
